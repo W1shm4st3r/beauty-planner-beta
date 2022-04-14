13ecd5a1ce69663338eea08d8aab670ee38f18ef
Filip Roszkowski <88441133+W1shm4st3r@users.noreply.github.com>
2022-04-14 10:26:59 +0100

    Update CosmeticEditor.java

--- src/com/iTensor/menu/options/CosmeticEditor.java
+++ src/com/iTensor/menu/options/CosmeticEditor.java
@@ -13,62 +13,62 @@ public class CosmeticEditor {
         Scanner scanner = new Scanner(System.in);
         try {
             if (cosmeticsList.getCosmeticList().isEmpty()) {
-                System.out.println("Lista jest pusta!");
+                System.out.println("The list is empty!");
             } else {
-                System.out.println("Na pewno? y/n");
+                System.out.println("Are you sure? y/n");
                 String decision = scanner.next();
                 if (decision.equals("y") || decision.equals("Y")) {
-                    System.out.print("Podaj ID kosmetyku, ktory chcesz edytowac: ");
+                    System.out.print("Type ID of a cosmetic you want to edit: ");
                     int id = scanner.nextInt();
                     Cosmetic c = cosmeticsList.getCosmeticById(id);
-                    System.out.println("Ktora ceche chcesz edytowac?");
-                    System.out.println("1. Nazwa");
-                    System.out.println("2. Data otwarcia");
-                    System.out.println("3. Data waznosci");
-                    System.out.println("4. Liczba aplikacji");
-                    System.out.print("Opcja: ");
+                    System.out.println("What feature you want to edit?");
+                    System.out.println("1. Name");
+                    System.out.println("2. Opening date");
+                    System.out.println("3. Validity term");
+                    System.out.println("4. Number of applications");
+                    System.out.print("Opction: ");
                     int option = scanner.nextInt();
                     switch (option) {
                         case 1 -> {
-                            System.out.print("Nazwa: ");
+                            System.out.print("Name: ");
                             String name = scanner.next();
                             c.setName(name);
                         }
                         case 2 -> {
-                            System.out.print("Data otwarcia: ");
+                            System.out.print("Opening date: ");
                             String date = scanner.next();
                             c.setOpeningDate(date);
                         }
                         case 3 -> {
-                            System.out.print("Termin waznosci: ");
+                            System.out.print("Validity term: ");
                             String term = scanner.next();
                             c.setValidityTerm(term);
                         }
                         case 4 -> {
-                            System.out.println("1. Zwieksz o 1");
-                            System.out.println("2. Zmniejsz o 1");
-                            System.out.println("3. Ustaw liczbe aplikacji");
-                            System.out.print("Opcja: ");
+                            System.out.println("1. Increase by 1");
+                            System.out.println("2. Decrease by 1");
+                            System.out.println("3. Set the number of applications");
+                            System.out.print("Option: ");
                             int newOption = scanner.nextInt();
                             int old;
                             switch (newOption) {
                                 case 1 -> {
                                     old = c.getApplications();
                                     c.setApplications(c.getApplications() + 1);
-                                    System.out.println("Zmieniono liczbe aplikacji z " + old + " na " + c.getApplications());
+                                    System.out.println("Number of applications change from " + old + " to " + c.getApplications());
                                 }
                                 case 2 -> {
                                     old = c.getApplications();
                                     c.setApplications(c.getApplications() - 1);
-                                    System.out.println("Zmieniono liczbe aplikacji z " + old + " na " + c.getApplications());
+                                    System.out.println("Number of applications change from " + old + " to " + c.getApplications());
                                 }
                                 case 3 -> {
                                     old = c.getApplications();
                                     int number;
-                                    System.out.print("Nowa liczba aplikacji: ");
+                                    System.out.print("New number of applications: ");
                                     number = scanner.nextInt();
                                     c.setApplications(number);
-                                    System.out.println("Zmieniono liczbe aplikacji z " + old + " na " + c.getApplications());
+                                    System.out.println("Number of applications change from " + old + " to " + c.getApplications());
                                 }
                             }
                         }
@@ -76,7 +76,7 @@ public class CosmeticEditor {
                 }
             }
         } catch (InputMismatchException e) {
-            System.out.println("Podaj liczbe!");
+            System.out.println("You have to type a number!");
         }
     }
 }
